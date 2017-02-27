0b3cf2457a41039e8d7413352bed5f5ca93c6949
Firefly4266 <johnathon@Icon-Base.(none)>
2017-02-26 20:41:45 -0800

    added a parameter route

--- app.js
+++ app.js
@@ -6,9 +6,15 @@ let app = express();
 let port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<html lang="en"><head><meta charset="UTF-8"/><title>Document</title></head><body><h1>Hello World!!.</h1></body></html>');
+  res.send('<html><head></head><body><h1>Hello World!!.</h1></body></html>');
 });
 
+//With this route we add a parameter called id (:id) We then add it to the html using (req.params.id).  The (.id) corresponds with the (:id) in this case. With this pattern anything entered in the request url after the specified address (/person/) will be rendered to the page at (req.params.id).
+app.get('/person/:id', (req, res) => {
+  res.send('<html><head></head><body><h1>Person: ' + req.params.id + '</h1></body></html>');
+});
+
+//This route will map to the address (/api) and respond with a json string produced from the object by using res.json.
 app.get('/api', (req, res) => {
   res.json({ firstname: 'John', lastname: 'Doe' });
 });
